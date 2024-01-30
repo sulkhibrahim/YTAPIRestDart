7148a2e5602f7a50fd041f2f3b3cc61813f32341
agusibrahim <pymobile123@gmail.com>
2024-01-30 23:31:39 +0700

    first commit

--- routes/index.dart
+++ routes/index.dart
@@ -53,36 +53,8 @@ dynamic streamVideoToJson(VideoStreamInfo streamInfo) {
 }
 
 Future<Response> onRequest(RequestContext context) async {
-  var ytid = context.request.uri.queryParameters["id"];
-  dynamic body = {"error": false, "code": 200, "data": {}};
-  if (ytid == null) {
-    body["error"] = true;
-    body["code"] = 401;
-    body["msg"] = "paramater 'id' required. please pass youtube id";
-    return Response.json(body: body);
-  }
-  try {
-    var yt = YoutubeExplode();
-    var video = await yt.videos.get(ytid);
-    body["data"] = {
-      "title": video.title,
-      "desc": video.description,
-      "length": video.duration?.inSeconds ?? 0,
-      "author": video.author,
-      "thumb": video.thumbnails.mediumResUrl,
-      "publish_date": video.publishDate?.toIso8601String() ?? DateTime(2000),
-      "id": video.id.value,
-      "channel_id": video.channelId.value,
-      "is_live": video.isLive,
-      "keywords": video.keywords,
-    };
-  } catch (e) {
-    body["error"] = true;
-    body["code"] = 500;
-    body["msg"] = e.toString();
-  }
-
-  return Response.json(body: body);
+  var body = "It's working. use /detail?id=yt_id_here or /stream?id=yt_id_here";
+  return Response(body: body);
 }
 /*
 
